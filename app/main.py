185b17f874bc4be4f151b6dc7b70b894539ae0fe
Lukmon Abdulsalam <40471908+litmus-zhang@users.noreply.github.com>
2024-05-25 17:50:47 +0100

    added graphql endpoint (#7)

--- app/main.py
+++ app/main.py
@@ -3,19 +3,24 @@ from dotenv import load_dotenv
 from app.router import router
 from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin as fa
+from strawberry.fastapi import GraphQLRouter
+from models.graphql import schema
 from config.config import get_settings
 load_dotenv()
 app = FastAPI()
 app.include_router(router)
 fa.initialize_app()
 
+graphql_app = GraphQLRouter(schema=schema, graphql_ide="apollo-sandbox")
+app.include_router(graphql_app, prefix="/graphql")
+
 settings = get_settings()
 
-origin=[settings.frontend_url, "*"]
+origin=["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[origin],
     allow_credentials=True,
-    allow_method=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
--- config/helper.py
+++ config/helper.py
@@ -1,6 +1,7 @@
 from firebase_admin import db
 
-brands = db.collections("brands")
+# brands = db.collections()
+brands = "Brands in Firestore"
 
 
 # create a collection for Brands
